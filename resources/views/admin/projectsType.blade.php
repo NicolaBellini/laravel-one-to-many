1d3e332978ab80ca109defcff4e83dc91d585ac0
NicolaBellini <bellininicola4366@gmail.com>
2024-05-24 15:47:22 +0200

    [add] content in projectsType

--- resources/views/admin/projectsType.blade.php
+++ resources/views/admin/projectsType.blade.php
@@ -5,4 +5,30 @@
 
 <h1>Projects type</h1>
 
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">tipo</th>
+      <th scope="col">progetti</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($types as $type)
+        <tr>
+            <td>{{ $type->name }}</td>
+            <td>
+                <ul>
+                    @foreach ($type->projects as $project)
+                        <li>{{ $project->name }}</li>
+                    @endforeach
+                </ul>
+            </td>
+        </tr>
+    @endforeach
+
+  </tbody>
+</table>
+
 @endsection
